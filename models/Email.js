e30dee4a0fc3a38a2724f909ae538b240be9249a
siskalim <siska.lim6868@gmail.com>
2025-06-20 15:35:47 +0700

    Update Email.js

--- models/Email.js
+++ models/Email.js
@@ -1,10 +1,13 @@
 const mongoose = require('mongoose');
 
 const emailSchema = new mongoose.Schema({
-  to: { type: String, required: true },
-  subject: { type: String, required: true },
-  message: { type: String, required: true },
-  receivedAt: { type: Date, default: Date.now }
+  to: String,
+  subject: String,
+  message: String,
+  receivedAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Email', emailSchema);
